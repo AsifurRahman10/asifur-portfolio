66e6a737e286bdbc7a3934a986c5b9ad5915c4c0
AsifurRahman10 <1720105@iub.edu.bd>
2025-02-06 17:12:22 +0600

    project section design added

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,19 +1,63 @@
+import { ProjectCard } from "../../Components/ProjectCard";
+import project1 from "../../assets/projects/p1.png";
+import project2 from "../../assets/projects/p2.png";
+import project3 from "../../assets/projects/p3.png";
+import project4 from "../../assets/projects/p4.png";
+
 export default function Projects() {
   const projectsData = [
     {
       title: "NextGen Hunt",
+      image: project1,
       description:
-        "NextGen Hunt is a platform where users can discover and share the latest technology, including web apps, AI tools, software, games, and mobile apps. It features role-based access (User, Moderator, Admin) and offers functionalities such as voting, reporting, product reviews, dashboard management, and coupon system integration. The site is fully responsive and provides a seamless user experience.",
+        "NextGen Hunt is a platform where users can discover and share the latest technology, including web apps, AI tools, software, games, and mobile apps. It features role-based access and functionalities such as voting, reporting, product reviews, dashboard management, and coupon system integration.",
       technologies: [
         "React",
         "Node.js",
         "Express.js",
-        "Firebase Authentication",
         "MongoDB",
+        "Firebase",
         "Stripe",
       ],
     },
-    {},
+    {
+      title: "ArtifactArcade",
+      image: project2,
+      description:
+        "ArtifactArcade is a web application dedicated to tracking historical artifacts. Users can explore, contribute, and engage with a collection of artifacts, fostering community participation in preserving historical knowledge.",
+      technologies: ["React", "Firebase", "Express.js"],
+    },
+    {
+      title: "VisaEase",
+      image: project3,
+      description:
+        "VisaEase is a Visa Navigator Portal designed to simplify visa management. Travelers can check visa requirements, apply for visas online, and track applications effortlessly.",
+      technologies: [
+        "React",
+        "Node.js",
+        "Express.js",
+        "Firebase",
+        "Tailwind CSS",
+      ],
+    },
+    {
+      title: "Lingo-Bingo",
+      image: project4,
+      description:
+        "Lingo-Bingo is an interactive e-learning platform designed to make learning the German language fun and accessible. With a rich vocabulary bank, expert support, and engaging lessons, users can master German at their own pace.",
+      technologies: ["React", "Firebase", "Tailwind CSS"],
+    },
   ];
-  return <div>Projects</div>;
+
+  return (
+    <div className="w-full min-h-screen">
+      <h2 className="text-white text-center text-4xl lg:text-6xl">Projects</h2>
+
+      <div className="space-y-6 mt-10 mx-auto w-5/6 lg:w-2/3">
+        {projectsData.map((project, idx) => (
+          <ProjectCard key={idx} project={project} />
+        ))}
+      </div>
+    </div>
+  );
 }
